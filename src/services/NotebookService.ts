9a2f9dc2a67c614bb8431711a470ff3e196313ff
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-14 23:17:16 -0300

    feat: Creates the getAllByName method in NotebookService

--- src/services/NotebookService.ts
+++ src/services/NotebookService.ts
@@ -7,4 +7,15 @@ export default class NotebookService {
     return notebooks;
   }
 
+  static async getAllByName(name: string): Promise<INotebook[]> {
+    const notebooks = await NotebookScraping.getAllNotebooks();
+    const notebooksFilteredByName = notebooks.filter((item: INotebook) =>
+      item.title?.toLowerCase().includes(name.toLowerCase()),
+    );
+    const notebooksSortedByValue = notebooksFilteredByName.sort(
+      (a: INotebook, b: INotebook) => a.price.value - b.price.value,
+    );
+    return notebooksSortedByValue;
+  }
+
 }
