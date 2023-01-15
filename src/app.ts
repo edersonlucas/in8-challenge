14fdcce3ae90a45716ec8ebc36d4eae468de00b1
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-14 23:55:51 -0300

    feat: Add the notebookRouter in the app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import ErrorMiddleware from './middleware/ErrorMiddleware';
+import notebookRouter from './routes/NotebookRouter';
 
 const app = express();
+app.use(notebookRouter);
+
 app.use(ErrorMiddleware.error);
 
 export default app;
