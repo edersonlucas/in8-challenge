8ac8aabfde84ca998e8a53605db674ec28a56637
Ederson Lucas <edersonlucas@outlook.com.br>
2023-01-14 21:57:29 -0300

    feat: Creates the getNotebookDetailById method in NotebookScraping

--- src/scraping/NotebookScraping.ts
+++ src/scraping/NotebookScraping.ts
@@ -1,5 +1,7 @@
 import puppeteer from 'puppeteer';
 import INotebook from '../interfaces/INotebook';
+import INotebookDetails from '../interfaces/INotebookDetails';
+import IVersionNotebook from '../interfaces/IVersionNotebook';
 import ErrorGenerator from '../utils/ErrorGenerator';
 
 export default class NotebookScraping {
@@ -53,4 +55,69 @@ export default class NotebookScraping {
     }
   }
 
+  static async getNotebookDetailById(id: number): Promise<INotebookDetails> {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1280, height: 800 });
+    await page.goto(
+      `https://webscraper.io/test-sites/e-commerce/allinone/product/${id}`,
+    );
+    try {
+      await page.waitForSelector('.caption');
+      const title =
+        (await page.$eval(
+          '.caption > h4:nth-child(2)',
+          (element) => element.textContent,
+        )) || '';
+      const description =
+        (await page.$eval(
+          '.caption > .description',
+          (element) => element.textContent,
+        )) || '';
+      const rating = {
+        qtyReviews: Number(
+          await page.$eval('.ratings > p', (element) =>
+            element.textContent?.replace(' reviews', ''),
+          ),
+        ),
+        ratingLevel: await page.$$eval(
+          '.glyphicon-star',
+          (elements) => elements.length,
+        ),
+      };
+      const versions: IVersionNotebook[] = await page.$$eval(
+        '.swatches > button',
+        (elements) =>
+          elements.map((element) => {
+            element.click();
+            const price = {
+              value: Number(
+                document.querySelector('.price')?.textContent?.replace('$', ''),
+              ),
+              currency: 'USD',
+            };
+            const hdd =
+              document.querySelector('.btn-primary')?.textContent || '';
+            const isAvailable = !document
+              .querySelector('.btn-primary')
+              ?.className.includes('disabled');
+            return {
+              price,
+              hdd,
+              available: isAvailable,
+            };
+          }),
+      );
+      await browser.close();
+      return {
+        title,
+        description,
+        versions,
+        rating,
+      };
+    } catch (e) {
+      await browser.close();
+      throw new ErrorGenerator(404, 'Notebook not found!');
+    }
+  }
 }
